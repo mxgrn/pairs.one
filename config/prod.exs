7f9586d9fcd1907889aae7913044599442601e0e
Max Gorin <max@goodbitlabs.com>
2016-10-12 20:35:12 +0200

    Set port from system env

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pairs_one, PairsOne.Endpoint,
   http: [port: 8070],
-  url: [host: "pairs.one"],
+  url: [host: System.get_env("APP_HOST")],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
