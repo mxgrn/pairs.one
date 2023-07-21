88ed31c65f08ead75386dfcd859334dac7d517bc
Max Gorin <regzok@gmail.com>
2023-07-21 13:51:12 +0300

    Update Phoenix

--- mix.exs
+++ mix.exs
@@ -34,14 +34,14 @@ defmodule PairsOne.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.6"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix, "~> 1.6.11"},
+      {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.3 or ~> 0.2.9"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 0.5"},
       {:gettext, "~> 0.20"},
       {:plug_cowboy, "~> 2.5"},
+      {:phoenix_live_view, "~> 0.18.2"},
 
       # Sentry
       {:sentry, "~> 8.0"},
